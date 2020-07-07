4f0a971378104a45a920fd4994b6a07897876076
jhaemin <io@jhaemin.com>
2020-07-07 15:32:17 +0900

    feat(sign up): always return true

--- server/src/api/sign-up.ts
+++ server/src/api/sign-up.ts
@@ -1,7 +1,41 @@
+import { UserInfo } from '@/modules/database/schema/user'
 import express from 'express'
 
 const router = express.Router()
 
-router.post('', (req, res) => {})
+type FieldResponse = {
+  res: boolean
+  err: string | null
+}
+
+export type SignUpResponse = {
+  [K in keyof UserInfo]: FieldResponse
+}
+
+// TODO: Use body validation middleware
+router.post('/sign-up', (req, res) => {
+  res.json({
+    id: {
+      res: true,
+      err: null,
+    },
+    password: {
+      res: true,
+      err: null,
+    },
+    email: {
+      res: true,
+      err: null,
+    },
+    name: {
+      res: true,
+      err: null,
+    },
+    phone: {
+      res: true,
+      err: null,
+    },
+  } as SignUpResponse)
+})
 
 export { router as signUpRouter }
