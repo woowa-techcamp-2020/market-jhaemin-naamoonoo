9e4c2c28b27d05353810a548ea09716f87dd8a6b
jhaemin <io@jhaemin.com>
2020-07-07 15:31:26 +0900

    test: test sign up

---
+++ server/src/api/__TEST__/sign-up.spec.ts
@@ -0,0 +1,89 @@
+import { SignUpResponse } from '../sign-up'
+import { UserInfo } from '../../modules/database/schema/user'
+import { app } from '../../app'
+import request from 'supertest'
+
+test('Sign up with all valid information should be passed', (done) => {
+  // given
+  const validUserInfo: UserInfo = {
+    id: 'fameu5e',
+    password: '12345678',
+    email: 'io@jhaemin.com',
+    name: '장해민',
+    phone: '010-5520-3618',
+  }
+
+  const expectedResult: SignUpResponse = {
+    id: {
+      res: true,
+      err: null,
+    },
+    password: {
+      res: true,
+      err: null,
+    },
+    email: {
+      res: true,
+      err: null,
+    },
+    name: {
+      res: true,
+      err: null,
+    },
+    phone: {
+      res: true,
+      err: null,
+    },
+  }
+
+  request(app)
+    .post('/sign-up')
+    .send(validUserInfo)
+    .then((response) => {
+      expect(response.body).toEqual(expectedResult)
+      done()
+    })
+})
+
+test('Sign up with short id should not be passed', (done) => {
+  // given
+  const validUserInfo: UserInfo = {
+    id: 'fam',
+    password: '12345678',
+    email: 'io@jhaemin.com',
+    name: '장해민',
+    phone: '010-5520-3618',
+  }
+
+  const expectedResult: SignUpResponse = {
+    id: {
+      res: false,
+      // TODO: Use constant error message
+      err: '',
+    },
+    password: {
+      res: true,
+      err: null,
+    },
+    email: {
+      res: true,
+      err: null,
+    },
+    name: {
+      res: true,
+      err: null,
+    },
+    phone: {
+      res: true,
+      err: null,
+    },
+  }
+
+  request(app)
+    .post('/sign-up')
+    .send(validUserInfo)
+    .then((response) => {
+      expect(response.body).toEqual(expectedResult)
+      done()
+    })
+})
