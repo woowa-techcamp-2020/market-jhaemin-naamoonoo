082ab1dd37369dee1aa205717f7cda2d2f549c73
naamoonoo <naamoonoo.y@gmail.com>
2020-07-08 10:47:53 +0900

    feat: split the error msg(#26)

---
+++ server/src/errors.ts
@@ -0,0 +1,18 @@
+export enum ErrMsg {
+  noneExistedUser = '존재하지 않는 아이디입니다. 아이디를 확인해주세요.',
+  wrongPassword = '비밀번호가 옳지 않습니다. 비밀번호를 확인해주세요.',
+  emptyUserId = '아이디를 입력해 주세요.',
+  emptyPassword = '비밀번호를 입력해 주세요.',
+
+  emptyPasswordCheck = '비밀번호 확인을 위해 한번 더 입력해 주세요.',
+  emptyEmail = '이메일 주소를 입력해 주세요.',
+  emptyName = '이름을 입력해 주세요.',
+  emptyPhone = '휴대폰 번호를 입력해 주세요.',
+  noneCheckEssential = '회원가입을 위해 필수항목에 동의해주세요.',
+  invalidUserId = '아이디는 영문과 숫자로 4자~20자 사이로 입력해 주세요.',
+  invalidPassword = '비밀번호는 영문과 숫자를 포함하여 8~20자로 입력해 주세요.',
+  differentPassword = '위 비밀번호와 일치하지 않습니다. 다시 입력해 주세요.',
+  invalidName = '이름에 특수문자, 숫자는 입력하실 수 없습니다. 다시 입력해 주세요.',
+  invalidEmail = '도메인의 형태가 옳지 않습니다. 다시 입력해 주세요.',
+  invalidPhone = '휴대폰 번호의 형태가 옳지 않습니다. 다시 입력해 주세요.',
+}
