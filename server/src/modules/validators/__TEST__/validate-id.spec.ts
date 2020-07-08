7bc8115b9bed16ec1c925ebe444046f7a34c92ca
naamoonoo <naamoonoo.y@gmail.com>
2020-07-08 10:52:17 +0900

    chore: update func name

--- server/src/modules/validators/__TEST__/validate-id.spec.ts
+++ server/src/modules/validators/__TEST__/validate-id.spec.ts
@@ -1,4 +1,4 @@
-import { validateId } from '../validate-id'
+import { validateUserId } from '../validate-user-id'
 
 // truthy
 test('ID has 4 to 20 length, consists of only lowercase english, number, or _, - should be pass', () => {
@@ -6,7 +6,7 @@ test('ID has 4 to 20 length, consists of only lowercase english, number, or _, -
   const validId = '-woowabros6_'
   const expectedResult = true
   //when
-  const testResult = validateId(validId)
+  const testResult = validateUserId(validId)
   //then
   expect(testResult).toBe(expectedResult)
 })
@@ -17,7 +17,7 @@ test('ID has less then 4 length should be fail', () => {
   const invalidId = 'abc'
   const expectedResult = false
   //when
-  const testResult = validateId(invalidId)
+  const testResult = validateUserId(invalidId)
   //then
   expect(testResult).toBe(expectedResult)
 })
@@ -27,7 +27,7 @@ test('ID has more then 20 length should be fail', () => {
   const invalidId = 'abcdefghijklmnopqrstuvwxyz'
   const expectedResult = false
   //when
-  const testResult = validateId(invalidId)
+  const testResult = validateUserId(invalidId)
   //then
   expect(testResult).toBe(expectedResult)
 })
@@ -37,7 +37,7 @@ test('ID has uppercase character should be fail', () => {
   const invalidId = 'woowaBros44'
   const expectedResult = false
   //when
-  const testResult = validateId(invalidId)
+  const testResult = validateUserId(invalidId)
   //then
   expect(testResult).toBe(expectedResult)
 })
@@ -47,7 +47,7 @@ test('ID has special character excpet for - or _ should be fail', () => {
   const invalidId = 'woowa!'
   const expectedResult = false
   //when
-  const testResult = validateId(invalidId)
+  const testResult = validateUserId(invalidId)
   //then
   expect(testResult).toBe(expectedResult)
 })
