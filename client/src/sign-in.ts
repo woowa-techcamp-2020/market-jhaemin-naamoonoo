60179fa0db4b3a2e3c55b93bdb2468f86bb20710
naamoonoo <naamoonoo.y@gmail.com>
2020-07-09 11:10:54 +0900

    feat: add eventListener to sign-in

--- client/src/sign-in.ts
+++ client/src/sign-in.ts
@@ -1,36 +1,98 @@
 import { fetchWrapper } from './main'
+import Validator from '../../server/src/modules/validators'
 
-// const res = fetchWrapper('POST', '/sign-up', {
-//   userId: 'test4444',
-//   password: 'test4444',
-//   name: '응복이',
-//   phone: '010-3333-3333',
-//   email: 'kanwkfe@naewffn.com',
-// })
-const inputs = Array.from(document.querySelectorAll('.input-field'))
 const signInButton = document.querySelector('.sign-in-btn')
 
-const onSubmitHandler = (e) => {
-  console.log(e)
-  console.log(inputs)
-
-  const userIdInput = inputs.find((input) => input.querySelector('.user-id'))
-  const passwordInput = inputs.find((input) => input.querySelector('.password'))
-  const body = {
-    userId: userIdInput.textContent,
-    password: passwordInput.textContent,
-  }
-  console.log(body)
-}
-// const res = fetchWrapper('POST', '/sign-in', {
-//   userId: 'test4444',
-//   password: 'test4444',
-// })
-
-signInButton.addEventListener('submit', (e) => {
+const onSubmitHandler = async (e: Event) => {
   e.preventDefault()
-  alert('submitted')
+  const userIdWrapper = document.querySelector('.input-wrapper.id')
+  const passwordWrapper = document.querySelector('.input-wrapper.pw')
 
-  console.log(e.target)
-  onSubmitHandler(e)
+  const userIdInput = userIdWrapper.querySelector('input') as HTMLInputElement
+  const passwordInput = passwordWrapper.querySelector(
+    'input'
+  ) as HTMLInputElement
+  const body = {
+    userId: userIdInput.value,
+    password: passwordInput.value,
+  }
+
+  const res = await fetchWrapper('POST', '/sign-in', body)
+  if (res['userId']) {
+    userIdWrapper.classList.add('invalid-input')
+    const errorMsg = userIdWrapper.querySelector('.msg-text')
+    errorMsg.textContent = res['userId'].error
+  }
+
+  if (res['password']) {
+    passwordWrapper.classList.add('invalid-input')
+    const errorMsg = userIdWrapper.querySelector('.msg-text')
+    errorMsg.textContent = res['password'].error
+  }
+}
+
+signInButton.addEventListener('click', onSubmitHandler)
+
+const userIdInput = document.querySelector(
+  '.input-wrapper.id input'
+) as HTMLInputElement
+
+const validationOnBlur = (e: Event) => {
+  const inputElement = e.target
+  if (!(inputElement instanceof HTMLInputElement)) {
+    return
+  }
+
+  const wrapper = inputElement.parentElement
+  const inputText = inputElement.value
+  const inputName = inputElement.name
+  const inputNameKr = inputElement.placeholder
+
+  const { validator, error } = Validator[inputName]
+  if (!inputText) {
+    wrapper.classList.remove('valid-input')
+    wrapper.classList.add('invalid-input')
+    const errorMsg = wrapper.querySelector('.msg-text')
+    errorMsg.textContent = `${inputNameKr} 값이 비어있어요. 입력해주세요.`
+    return
+  }
+
+  if (validator(inputText)) {
+    wrapper.classList.add('valid-input')
+    wrapper.classList.remove('invalid-input')
+  } else {
+    wrapper.classList.remove('valid-input')
+    wrapper.classList.add('invalid-input')
+    const errorMsg = wrapper.querySelector('.msg-text')
+    errorMsg.textContent = error
+  }
+}
+
+userIdInput.addEventListener('blur', validationOnBlur)
+
+const validationOnChange = (e: Event) => {
+  const inputElement = e.target
+  if (!(inputElement instanceof HTMLInputElement)) {
+    return
+  }
+
+  const wrapper = inputElement.parentElement
+  if (!wrapper.classList.contains('invalid-input')) {
+    return
+  }
+
+  const inputText = inputElement.value
+  const inputName = inputElement.name
+  const { validator } = Validator[inputName]
+  if (validator(inputText)) {
+    wrapper.classList.add('valid-input')
+    wrapper.classList.remove('invalid-input')
+  }
+}
+
+userIdInput.addEventListener('input', validationOnChange)
+const inputs = Array.from(document.querySelectorAll('.input-text'))
+inputs.forEach((input) => {
+  input.addEventListener('blur', validationOnBlur)
+  input.addEventListener('input', validationOnChange)
 })
